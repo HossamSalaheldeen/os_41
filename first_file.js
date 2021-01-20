5f342353b79c9b9f0dda35fc580c9f66160ed811
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-01-20 21:22:31 +0200

    consol log first file

--- first_file.js
+++ first_file.js
@@ -2,3 +2,5 @@ console.log("first file");
 
 //track the project folder by local git  ---- >git init 
 //add folder to staging area ---------------- >git add .
+// create log history for code -------------- >git commit -m "msg"
+//check modified file in working directory >git status 
